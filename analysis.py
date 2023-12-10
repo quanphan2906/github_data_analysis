4a5cde11bbe0b2c6d71667c2d190734224a01ce7
Quan Phan <hoangquanphan2906@gmail.com>
2023-12-10 13:23:38 +0200

    First-draft

--- analysis.py
+++ analysis.py
@@ -2,155 +2,216 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import MinMaxScaler
+from scipy.stats import chi2_contingency
 import streamlit as st
 
-data = pd.read_csv("data/repository_data_10k.csv")
 
-# take repos with top 3 licenses
-licenses_of_interest = [
-    "MIT License",
-    "GNU General Public License v3.0",
-    "Apache License 2.0",
-]
-filtered_data = data[data["licence"].isin(licenses_of_interest)]
-filtered_data.replace(
-    {
-        "GNU General Public License v3.0": "GNU GPL v3.0",
-        "Apache License 2.0": "Apache 2.0",
-    },
-    inplace=True,
-)
-licenses_of_interest = [
-    "MIT License",
-    "GNU GPL v3.0",
-    "Apache 2.0",
-]
-
-project_metrics = ["stars_count", "forks_count", "pull_requests", "commit_count", "watchers"]
-
-
-top_20_percent_data_map = {}
-top_20_percent_dfs = []
-
-for license_type in licenses_of_interest:
-    license_data = filtered_data[filtered_data["licence"] == license_type]
-
-    percentile_80 = license_data["stars_count"].quantile(0.80)
-    top_20_percent = license_data[license_data["stars_count"] >= percentile_80]
-    top_20_percent_data_map[license_type] = top_20_percent
-    top_20_percent_dfs.append(top_20_percent)
-
-top_20_percent_df = pd.concat(top_20_percent_dfs)
-
-
-license_counts_percentage = filtered_data["licence"].value_counts(normalize=True) * 100
-
-
-def license_value_counts():
-    fig, ax = plt.subplots()
-    sns.barplot(
-        x=license_counts_percentage.index,
-        y=license_counts_percentage.values,
-        palette="deep",
-        ax=ax,
-    )
-    ax.set_title("Percentage Distribution of Projects by License")
-    ax.set_xlabel("License")
-    ax.set_ylabel("Percentage of Projects (%)")
-    plt.xticks(rotation=45)
-
-    st.pyplot(fig)
-
-
-def generate_grouped_bar_chart(data=top_20_percent_df, title="Average value of metrics by license"):
-    grouped_means = data.groupby("licence")[project_metrics].mean().reset_index()
-
-    # Melt the DataFrame to long format for seaborn's barplot
-    melted_data = grouped_means.melt(
-        id_vars="licence", value_vars=project_metrics, var_name="Metric", value_name="Mean"
-    )
-
-    # Create the grouped bar chart
-    fig, ax = plt.subplots(figsize=(12, 6))
-    sns.barplot(x="Metric", y="Mean", hue="licence", data=melted_data, ax=ax)
-    ax.set_title(title)
-    ax.set_xlabel("Metric")
-    ax.set_ylabel("Mean Value")
-    plt.xticks(rotation=45)
-
-    st.pyplot(fig)
-
-
-def generate_box_plot_and_summary(metric, data):
-    if metric not in project_metrics:
-        st.error(f"Invalid metric. Please choose from {project_metrics}.")
-        return
-
-    # Generate Box Plot
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.boxplot(y="licence", x=metric, data=data)
-    ax.set_xscale("log")
-    ax.set_title(f"Box Plot of {metric.capitalize().replace('_', ' ')} by License")
-    ax.set_ylabel("License")
-    ax.set_xlabel(metric.capitalize().replace("_", " "))
-    plt.yticks(rotation=45)
-    st.pyplot(fig)
-
-    # Generate Five-Number Summary Table
-    summary_df = pd.DataFrame()
-    for license_type in licenses_of_interest:
-        summary_series = data[data["licence"] == license_type][metric].describe()
-        summary_df[license_type] = summary_series
-
-    st.markdown(f"###### Five-Number Summary for {metric.replace('_', ' ').title()}")
-    st.table(summary_df)
-
-
-def generate_overlaid_histograms(data=filtered_data, metric="stars_count"):
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    for license_type in data["licence"].unique():
-        # Filter data for each license
-        license_data = data[data["licence"] == license_type]
-
-        # Plot histogram
-        ax.hist(license_data[metric], bins=30, alpha=0.5, label=license_type)
-
-    ax.set_title(f'Distribution of {metric.capitalize().replace("_", " ")} by License')
-    ax.set_xlabel(metric.capitalize().replace("_", " "))
-    ax.set_ylabel("Frequency")
-    ax.legend()
-
-    st.pyplot(fig)
-
-
-def generate_overlaid_kde_plots(data=filtered_data, metric="stars_count", use_log_scale=True):
-    fig, ax = plt.subplots(figsize=(10, 6))
-
-    for license_type in data["licence"].unique():
-        # Filter data for each license
-        license_data = data[data["licence"] == license_type][metric]
-
-        # Calculate IQR and determine bounds for outliers
-        Q1 = license_data.quantile(0.25)
-        Q3 = license_data.quantile(0.75)
-        IQR = Q3 - Q1
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-
-        # Filter out outliers
-        filtered_values = license_data[
-            (license_data >= lower_bound) & (license_data <= upper_bound)
+class Analysis:
+    def __init__(self) -> None:
+        self.data = pd.read_csv("data/repository_data_10k.csv")
+        self.numerical_columns = ["stars_count", "forks_count", "watchers", "pull_requests"]
+        self.license_types = [
+            "MIT License",
+            "GNU General Public License v3.0",
+            "Apache License 2.0",
         ]
 
-        # Plot KDE
-        sns.kdeplot(filtered_values, ax=ax, label=license_type, fill=True)
+    def compute_composite_score(self):
+        data = self.data
+        numerical_columns = self.numerical_columns
 
-    ax.set_title(
-        f'Distribution of {metric.capitalize().replace("_", " ")} by License (Outliers Removed)'
-    )
-    ax.set_xlabel(metric.capitalize().replace("_", " "))
-    ax.set_ylabel("Density")
-    ax.legend()
+        data[numerical_columns] = data[numerical_columns].fillna(data[numerical_columns].median())
 
-    st.pyplot(fig)
+        scaler = MinMaxScaler()
+        data_normalized = scaler.fit_transform(data[numerical_columns])
+        weights = np.ones(len(numerical_columns)) / len(numerical_columns)
+        composite_scores = np.dot(data_normalized, weights)
+        data["Composite_Score"] = composite_scores
+
+    def split_typical_popular(self):
+        data = self.data
+
+        # Determining the upper interquartile range for the composite score
+        Q3_composite = data["Composite_Score"].quantile(0.75)
+        IQR_composite = Q3_composite - data["Composite_Score"].quantile(0.25)
+        upper_bound_composite = Q3_composite + 1.5 * IQR_composite
+
+        # Classifying repositories as 'popular' if their composite score is beyond the upper interquartile range
+        data["is_popular"] = data["Composite_Score"] > upper_bound_composite
+
+        composite_outlier_category_counts = data["is_popular"].value_counts()
+        return composite_outlier_category_counts
+
+    def plot_top_licenses(self, top_count=5):
+        license_counts = self.data["licence"].value_counts(normalize=True) * 100
+        top_10_licenses = license_counts.round(1).head(top_count)
+
+        # Plot setup
+        fig, ax = plt.subplots(figsize=(10, 8))
+        bars = top_10_licenses.plot(kind="bar", ax=ax)
+
+        # Setting labels and title
+        ax.set_title(f"Top {top_count}  Most Used Licenses (by percentage)")
+        ax.set_xlabel("License")
+        ax.set_ylabel("Percentage of Projects")
+
+        plt.xticks(rotation=45)
+        ax.bar_label(bars.containers[0])
+        plt.tight_layout()
+
+        st.pyplot(fig)
+
+    def get_license_distribution(self):
+        data = self.data
+
+        popular = data[data["is_popular"]]
+        typical = data[data["is_popular"] == False]
+        license_distribution_outliers = (
+            popular["licence"].value_counts(normalize=True)[self.license_types] * 100
+        )
+        license_distribution_typical = (
+            typical["licence"].value_counts(normalize=True)[self.license_types] * 100
+        )
+
+        return license_distribution_outliers, license_distribution_typical
+
+    def plot_license_dist(self, license_distribution_outliers, license_distribution_typical):
+        # Ensure that both series have the same index in the correct order
+        index = self.license_types
+        license_distribution_outliers = license_distribution_outliers.reindex(index).fillna(0)
+        license_distribution_typical = license_distribution_typical.reindex(index).fillna(0)
+
+        fig, ax = plt.subplots()
+        grouped_data = pd.DataFrame(
+            {
+                "Popular Repositories": license_distribution_outliers,
+                "Typical Repositories": license_distribution_typical,
+            }
+        ).T
+
+        # Creating a grouped bar chart
+        grouped_data.plot(kind="bar", ax=ax, figsize=(10, 6))
+        ax.set_title("License Distribution Comparison")
+        ax.set_ylabel("Percentage")
+        ax.set_xlabel("Repository Type")
+
+        # Annotate the bars with the percentage values
+        for container in ax.containers:
+            ax.bar_label(container, fmt="%.1f%%", padding=3)
+
+        plt.xticks(rotation=0)
+
+        plt.tight_layout()
+        st.pyplot(fig)
+
+    def test_significance(self):
+        data = self.data
+
+        popular = data[data["is_popular"]]
+        typical = data[data["is_popular"] == False]
+
+        # MIT license
+        mit_popular = popular["licence"].value_counts().get("MIT License", 0)
+        mit_typical = typical["licence"].value_counts().get("MIT License", 0)
+        non_mit_popular = popular.shape[0] - mit_popular
+        non_mit_typical = typical.shape[0] - mit_typical
+
+        contingency_table = np.array(
+            [[mit_popular, non_mit_popular], [mit_typical, non_mit_typical]]
+        )
+
+        chi2, p, _, _ = chi2_contingency(contingency_table)
+
+        # Apache License 2.0
+        apache_popular = popular["licence"].value_counts().get("Apache License 2.0", 0)
+        apache_typical = typical["licence"].value_counts().get("Apache License 2.0", 0)
+        non_apache_popular = popular.shape[0] - apache_popular
+        non_apache_typical = typical.shape[0] - apache_typical
+
+        contingency_table_apache = np.array(
+            [[apache_popular, non_apache_popular], [apache_typical, non_apache_typical]]
+        )
+
+        chi2_apache, p_apache, _, _ = chi2_contingency(contingency_table_apache)
+
+        # Visualize the result
+        test_results = pd.DataFrame(
+            {
+                "License": ["MIT License", "Apache 2.0"],
+                "Chi-Squared": [chi2, chi2_apache],
+                "p-value": [p, p_apache],
+            }
+        )
+
+        return test_results
+
+    def get_and_plot_top_10_languages(self):
+        language_popularity = self.data["primary_language"].value_counts()
+        top_10_languages = language_popularity.head(10)
+        total_count = language_popularity.sum()
+        top_10_languages_percentage = (top_10_languages / total_count) * 100
+
+        # Plot setup
+        fig, ax = plt.subplots(figsize=(10, 6))
+        bars = top_10_languages_percentage.plot(kind="bar", ax=ax)
+
+        # Setting labels and title
+        ax.set_title("Top 10 Programming Languages by Percentage")
+        ax.set_xlabel("Programming Language")
+        ax.set_ylabel("Percentage")
+
+        # Add values on top of the bars
+        for bar in bars.patches:
+            ax.annotate(
+                f"{bar.get_height():.1f}%",
+                xy=(bar.get_x() + bar.get_width() / 2, bar.get_height()),
+                xytext=(0, 3),
+                textcoords="offset points",
+                ha="center",
+                va="bottom",
+            )
+
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        st.pyplot(fig)
+
+        return top_10_languages
+
+    def plot_lang_and_license(self, top_10_languages, license="MIT License"):
+        data = self.data
+
+        top_10_languages_list = top_10_languages.index.tolist()
+        filtered_data = data[data["primary_language"].isin(top_10_languages_list)]
+        license_distribution = (
+            filtered_data.groupby(["primary_language", "licence"]).size().unstack().fillna(0)
+        )
+        license_distribution_percentage = (
+            license_distribution.div(license_distribution.sum(axis=1), axis=0) * 100
+        )
+        license_distribution_percentage = license_distribution_percentage.round(1)
+        sorted_values = license_distribution_percentage[license].sort_values(ascending=False)
+
+        # Plot setup
+        fig, ax = plt.subplots(figsize=(8, 5))  # Adjust the figure size as needed
+
+        # Plotting for the specified license
+        bars = sorted_values.plot(kind="bar", ax=ax)
+        ax.set_xticklabels(sorted_values.index, rotation=45, fontsize=12)
+        ax.set_title(f"{license} Proportion by Programming Language", fontsize=14)
+        ax.set_ylabel("Proportion (%)", fontsize=12)
+        ax.set_xlabel("")
+
+        for bar in bars.patches:
+            ax.annotate(
+                f"{bar.get_height():.1f}%",
+                xy=(bar.get_x() + bar.get_width() / 2, bar.get_height()),
+                xytext=(0, 3),
+                textcoords="offset points",
+                ha="center",
+                va="bottom",
+            )
+
+        plt.tight_layout()
+        st.pyplot(fig)
--- main.py
+++ main.py
@@ -1,60 +1,129 @@
-"""
-Find and highlight an interesting insight or pattern 
-in the GitHub projects data and develop visualizations to showcase this.
-"""
-
 import streamlit as st
 import analysis
+import seaborn as sns
 
-st.title("Impact of License Types on Open Source Project Metrics")
+sns.set_theme()
+data = analysis.Analysis()
 
-st.header("Introduction")
+st.title("⌨️ Choice of license in open-sourced Github repositories ⌨️")
+
+st.markdown("Author: [Quan Phan](https://github.com/quanphan2906)")
+
+st.header("👋 Introduction")
+
+"In software engineering, especially in the open-source world, a license of a project provides important information on how people other than the authors can use, distribute, and modify the project."
+
+"In this analysis, I want to understand how the choice of license changes across different open-sourced projects and communities. More specifically, I want to answer three questions..."
 
 st.markdown(
-    "The goal of this analysis is to provide insights into how the **choice of license** might influence **project engagement and contribution** in the *open source* community."
+    """
+    1. What is the most popular license in the open-sourced world?
+    2. How does the choice of license differs for a popular, well-known project, compared to a regular-size open-source repo?
+    3. How does the choice of license differs across open-source communities (by programming language)?
+    """
+)
+
+"This analysis was inspired by my recent research and choice of license for a new open-source project."
+
+st.header("🍌 Data")
+st.markdown(
+    """
+The dataset is a sample of 10,000 data points, taken from the version 2 of the [Github Dataset](https://www.kaggle.com/datasets/nikhil25803/github-dataset/data) on Kaggle. Readers can find the description of the data in the Kaggle link.
+"""
+)
+
+st.header("🏆 Top choice licenses")
+
+data.plot_top_licenses(7)
+
+"It is impressive to see 50% of the projects use the MIT license. The runner-ups are Apache License 2.0 and GNU GPL v3.0."
+
+"To better understand this distribution, here is a brief overview of these three licenses' features:"
+
+st.markdown(
+    """
+__MIT license__ is known for its simplicity and permissive nature. It allows users to do almost anything they want with the code, even distributing, sublicensing, and selling copies of the software. The only major requirement is to include the original copyright and license notice in any copy of the software/source code.
+"""
 )
 
 st.markdown(
     """
-To that end, I examine the correlations between open source project licenses and key project metrics, including 
-- star counts
-- fork counts
-- pull requests
-- commit counts.
-"""
+    __Apache License 2.0__ is as permissive as MIT license, and users can do almost anything they want with the code. However, a distinctive feature of the Apache License is its explicit grant of patent rights from contributors to users, protecting users from patent litigation. In addition, it requires modifications to be documented, making it easier for subsequent users to understand what was altered.
+    """
 )
-
-"I focus on three popular licenses – the MIT License, GNU General Public License v3.0 (GPL 3.0), and Apache License 2.0. These licenses were chosen due to their popularity and distinct characteristics, offering a broad view of different open source licensing approaches."
-
-st.header("Data source")
 
 st.markdown(
     """
-The dataset is a sample of 10,000 data points, taken from the version 2 of the [Github Dataset](https://www.kaggle.com/datasets/nikhil25803/github-dataset/data) on Kaggle.
-"""
+    A key feature of __GNU GPL v3.0__ is its strong copyleft requirement. If you distribute modified versions of GPLv3-licensed software, you must also distribute the entire source code under GPLv3. This ensures that all modified versions of the software remain free and open.
+    """
 )
 
-st.header("Preliminary")
+"With this understanding, we can hypothesize the reasons behind the distribution we saw earlier. The MIT license, due to its simplicity and minimal requirements, becomes the most widely adopted license across the open-sourced community. The runner-ups, Apache  2.0 and GNU GPL v3.0, are also famous because they are as permissive as the MIT license, but are less appealing as the champion due to their additional requirements, including the modifications documentation requirement of Apache 2.0 and the strong copyleft of GNU GPL v3.0."
 
-f"""The majority of the repositories ({analysis.license_counts_percentage["MIT License"].round()}%) are licensed under the MIT license, which is well expected. The MIT license is known for its permissiveness, simplicity and compatibility with many other types of licenses, which makes it a popular choice among developers and organizations, all-size alike. While Apache 2.0 ({analysis.license_counts_percentage["Apache 2.0"].round()}%) and GNU GPL 3.0 ({analysis.license_counts_percentage["GNU GPL v3.0"].round()}%) are all popular options, their popularity is nowhere near the MIT license."""
+st.header("🍉 Prefered license types in popular vs. regular repositories")
 
-analysis.license_value_counts()
+st.subheader("Hypothesis")
+"One hypothesis that might spring off from our understanding of the licenses is this. MIT license might be more appealing to a typical, small-scale project due to its simplicity. Conversely, the Apache 2.0 License offers more protection against patent infringement claims and has explicit grants of patent rights. This might make it more appealing for larger and commercially-oriented projects, which could be more prevalent in the popular group."
 
-"Maybe we also do the distribution of the engagement metrics"
+st.subheader("Popular vs. regular repositories")
 
-st.header("License type correlates with popularity of repo")
-
-"""
-To start off our correlation analysis, let us focus on the average values of the engagement metrics across different license types. Consistently across all metrics, Apache 2.0 takes the lead, followed by GNU GPL 3.0 and MIT License, in that order.
-"""
-
-analysis.generate_grouped_bar_chart(analysis.filtered_data)
-analysis.generate_grouped_bar_chart(
-    analysis.top_20_percent_df,
-    title="Average value of metrics by license (top 20% most popular repos)",
+st.markdown(
+    """To examine this hypothesis, I first define what a popular repository is versus a typically small-scale one, and separate them into two groups: A popular (and mature) project is any project with the __weighted average number of stars, watchers, forks, and pull requests larger than the upper interquartile range of this metric__. The rest, I call regular projects. 
+    """
 )
 
-analysis.generate_box_plot_and_summary("stars_count", analysis.top_20_percent_df)
+data.compute_composite_score()
+composite_outlier_category_counts = data.split_typical_popular()
+typical = composite_outlier_category_counts.values[0]
+popu = composite_outlier_category_counts.values[1]
 
-analysis.generate_overlaid_histograms()
-analysis.generate_overlaid_kde_plots()
+f"By this definition, we have {typical} typical repositories ({typical / (typical + popu) * 100}%), and {popu} popular repositories ({popu / (typical + popu) * 100}%)."
+
+st.markdown(
+    """
+    In defining my proxy metric, I intentionally leave out the number of commits. My _assumption_ is that the number of commits does not necessarily correlate with the maturity of a project. A high number of commits might just mean a high number of bug fixes. Alternatively, beginner developers can push many commits without good reasons. I also use the upperquartile range of this proxy metric as a cutoff point because as a norm in the Stats community, anything beyond the upperquartile range is considered outliers.
+    """
+)
+
+st.subheader("Result")
+
+"While MIT license dominates in both the popular and regular-size project groups, the percentage is considerably higher in the latter (10 percentage unit difference). Among the three, only Apache 2.0 has a higher usage rate in the popular project group compared to the typical group."
+
+license_distribution_outliers, license_distribution_typical = data.get_license_distribution()
+data.plot_license_dist(license_distribution_outliers, license_distribution_typical)
+
+"This result confirms the direction of my initial hypothesis, that a small-scale project is more likely to use the MIT license and the popular, mature projects are more likely to use Apache 2.0. However, the magnitude of difference is not as significant as I expected."
+
+st.header("⌨️ Prefered license types across different open-sourced communities")
+
+"Here, I have no hypothesis to start out with, so let's begin by getting to know the largest programming communities:"
+
+top_10_languages = data.get_and_plot_top_10_languages()
+
+"This is a well-expected list. Let's see how the choice of license differs across the communities."
+
+data.plot_lang_and_license(top_10_languages)
+
+"Notice the high usage proportion of MIT license in Javascript and Typescript projects. This pattern well aligns with the strong culture of sharing and building upon others' work in the web development world (which heavily uses Javascript and Typescript). In addition, many large and influential projects in these languages (like Angular, React, Express.js) use the MIT License, setting a precedent for others in the community."
+
+data.plot_lang_and_license(top_10_languages, "Apache License 2.0")
+
+"Apache License 2.0 is significantly more popular in the Java community than the rest. The connection between these two are more subtle. One hypothesis is that since both Java and Apache 2.0 (with its explicit grants of patent rights) are widely used in the enterprise environments, there are more Java projects with Apache 2.0 than other languages."
+
+data.plot_lang_and_license(top_10_languages, "GNU General Public License v3.0")
+
+"Finally, we see GNU GPL v3.0 is more widely used in the C++ and Python communities than the rest, although the difference is not that significant."
+
+st.header("🍊 Conclusion")
+
+"In this analysis, I have explored and answered all the questions in the Introduction related to the choice of licenses. Here are some highlights..."
+
+st.markdown(
+    """
+    1. The most popular license in the open-sourced world is MIT license, followed by Apache 2.0 and GNU GPL v3.0.
+    2. Compared to large, well-known project, a small-scale project is more likely to use the MIT license. Conversely, Apache 2.0 is more popular for large projects than small ones.
+    3. MIT license has the highest usage proportion in the web development community, while Apache 2.0 is the most popular in the Java community.
+    """
+)
+
+"I hope you had fun following this analysis and it is somehow helpful for your choice of license in your next open-source project. Thanks for following along and will see you in an interview 😉"
